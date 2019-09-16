2c455ffb8e2e943bdbea45aab3fa1a886a9078b0
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-09-16 11:22:51 -0400

    Create Quickcodeex2.py

---
+++ Quickcodeex2.py
@@ -0,0 +1,12 @@
+def exercise_2():
+    largest_3digit = 999 # this is the largest possible 3 digit number
+    smallest_5digit = 10000 # this is the smallest possible 5 digit number
+
+    print(largest_3digit)
+    print(smallest_5digit)
+    print((largest_3digit + smallest_5digit)) # You could ALSO define another variable like
+    # sum = largest_3digit + smallest_5digit and then print(sum)
+
+
+if __name__ == "__main__":
+    exercise_2()
