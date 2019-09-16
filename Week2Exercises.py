51978e79832d5813867a2e6e8ff101171452beb5
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-09-15 22:18:23 -0400

    Create Week2Exercises.py

---
+++ Week2Exercises.py
@@ -0,0 +1,36 @@
+def exercise_1():
+    print("I am going to love this course")
+
+
+def exercise_2():
+    # largest 3 digit number is 999
+    largest_3 = 999
+    # smallest 5 digit number is 10000
+    smallest_5 = 10000
+
+    print("Largest 3 digit number:", largest_3, "\nSmallest 5 digit number:", smallest_5,
+          "\nSum:", (largest_3 + smallest_5))
+
+
+def exercise_3():
+    cost_1 = int(input("Cost?\n"))
+    num_items = int(input("How many?\n"))
+    print("Total cost is $", num_items * cost_1, sep='')
+
+
+def exercise_4():
+    c = float(input("Celsius temperature: "))
+    print("Fahrenheit equivalent is:", c * 9 / 5 + 32)
+
+
+def exercise_5():
+    x = float(input("Input x: "))
+    print("The value of y = 4x + 3 is", (4 * x + 3))
+
+
+if __name__ == "__main__":
+    exercise_1()
+    exercise_2()
+    exercise_3()
+    exercise_4()
+    exercise_5()
