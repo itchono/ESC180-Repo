8579c3d9445863fa95e6cbec6f0396556be64530
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-09-23 13:10:08 -0400

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -22,13 +22,15 @@ def is_cow_within_bounds(cow_position, boundary_points):
     boundary_points must be inputted as a list of 4 lists each containing 2 floats, each denoting the
     x,y coordinates of a rectangular boundary starting from the upper left corner and moving clockwise.
     cow_position must be inputted as a list of 2 floats, denoting the x,y coordinates of the cow
-    Assumes x+ is right and y+ is down
+    Assumes x+ is right and y+ is up
     """
-    int result = -1;
-    if  boundary_points[1][0] > cow_position[0] > boundary_points[0][0]:
-        # verfy x condition is satisfied
-    if boundary_points[3][1] > [
-
+    if  (boundary_points[1][0] > cow_position[0] > boundary_points[0][0])\
+    and (boundary_points[0][1] > cow_position[1] > boundary_points[3][1]):
+        # verfy x condition is satisfied and also y condition
+        return 1
+    else:
+        return 0
+# TBD: comment more and verify x+ and y+
 
 def find_cow_distance_to_boundary(cow_position, boundary_point):
     """
@@ -43,4 +45,4 @@ def find_time_to_escape(cow_speed, cow_distance):
     """
 
 if __name__ == "__main__":
-    print(find_euclidean_distance(3, 3, 2,5))
+    print(is_cow_within_bounds([3, 3], [[4, 4], [5, 4], [5, 2], [4, 2]]))
