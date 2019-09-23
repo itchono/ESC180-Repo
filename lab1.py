9d194c6b53262015895863b6ea8d94e1cd57d072
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-09-23 11:36:12 -0400

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,31 @@
+import math
+# ESC180 Lab 1
+# Connected Cows
+# DO NOT modify any function or argument names
+
+def find_euclidean_distance(x1, y1, x2, y2):
+    """
+    (float, float, float, float) -> float
+    find_euclidean_distance 
+    """
+    #function body
+
+
+def is_cow_within_bounds(cow_position, boundary_points):
+    """
+    Your docstring here
+    """
+    #function body
+
+
+def find_cow_distance_to_boundary(cow_position, boundary_point):
+    """
+    Your docstring here
+    """
+    #function body
+
+
+def find_time_to_escape(cow_speed, cow_distance):
+    """
+    Your docstring here
+    """
