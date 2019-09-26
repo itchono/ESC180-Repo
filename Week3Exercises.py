6320cb08ad6edd69619dc8efa47eb59c87ef310b
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-09-26 14:13:38 -0400

    Create Week3Exercises.py

---
+++ Week3Exercises.py
@@ -0,0 +1,36 @@
+import math
+
+# ex 1
+def change(m):
+    m0 = m
+    q = m//25
+    m %= 25
+
+    d = m//10
+    m %= 10
+
+    n = m//5
+    m %= 5
+
+    print(m0, "is equal to\n", q, "quarters\n", d, "dimes\n", n, "nickels\n", m, "pennies.")
+    
+# ex 2
+def round_num():
+    n = float(input("Enter float"))
+    print(((10*n)//1)/10) # rudimentary truncation
+    print(round(n, 1))
+
+# ex 3
+def trig():
+    d = math.pi*float(input("Angle in degrees"))/180
+    print(d)
+    print(math.sin(d), math.cos(d), math.tan(d))
+    
+
+
+
+if __name__ == "__main__":
+    change(67)
+    round_num()
+    trig()
+    
