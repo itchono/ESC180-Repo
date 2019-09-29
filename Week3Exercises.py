89137f2b4fb8e668daacbcf2ee453a54a4105963
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-09-29 12:39:10 -0400

    Update Week3Exercises.py

--- Week3Exercises.py
+++ Week3Exercises.py
@@ -18,19 +18,24 @@ def change(m):
     
 # ex 2
 def round_num():
-    n = float(input("Enter float"))
-    print(((10*n)//1)/10) # rudimentary truncation
+    n = float(input("Enter float\n"))
+    print((n*10)//1 / 10) # rudimentary truncation
     print(round(n, 1))
+	
 
 # ex 3
 def trig():
-    d = math.pi*float(input("Angle in degrees"))/180
-    print(d)
-    print("sin(", d, ") =", math.sin(d), "\ncos(", d, ") =", math.cos(d), "\ntan(", d, ") =",math.tan(d))
+    d = math.pi*float(input("Angle in degrees\n"))/180
+    print(round(d * 180 / math.pi, 2))
+    print("sin(", round(d * 180 / math.pi, 2), ") =", math.sin(d), "\ncos(", round(d * 180 / math.pi, 2), ") =", math.cos(d), "\ntan(", round(d * 180 / math.pi, 2), ") =",math.tan(d), sep = '')
     
 # ex 4
 def cdn_change(m):
     m0 = m
+	
+	# round to nearest 5
+    m = m0//5 * 5 + round(m0%5/5)
+	
     q = m//25
     m %= 25
 
@@ -39,14 +44,14 @@ def cdn_change(m):
 
     n = m//5
     m %= 5
-
-    if (m >= 3):
-        n += 1
-    print(m0, "is equal to\n", q, "quarters\n", d, "dimes\n", n, "nickels\n")
+		
+    print(m0, "is equal to\n", q, "quarters\n", d, "dimes\n", n, "nickels\n", "if you're in Canada.")
+	
+    
     
 if __name__ == "__main__":
     change(67)
     round_num()
     trig()
-    cdn_change(69)
+    cdn_change(92)
     
