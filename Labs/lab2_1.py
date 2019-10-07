c6eee33ab8bbb0d8722eeebe938ed62b6573415a
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-10-07 11:56:21 -0400

    Update lab2_1.py

--- Labs/lab2_1.py
+++ Labs/lab2_1.py
@@ -1,6 +1,22 @@
+from math import sqrt
+
 def vector_from_points(p1, p2):
-    ''' Fill in docstring
     '''
+	(list<float>, list<float>) -> list<float>
+	
+	vector_from_points returns an n-dimensional list corresponding to a vector represented by
+	p2 at the tail and p1 at the tip. p1 and p2 are lists with the same number of dimensions.
+	
+	>>>vector_from_points([0, 0], [1, 2])
+	[1, 2]
+    '''
+	
+    v = [] # initialize empty list for case n = 0
+	
+    for i in range (0, len(p1)):
+        v.append(p2[i] - p1[i])
+		
+    return v
 
 def vector_length(v):
     ''' Fill in docstring
@@ -34,3 +50,4 @@ if __name__ == "__main__":
     # test your vector operations here
     v1 = [0, -2, 3]
     v2 = [1, 1, 1]
+    print(vector_from_points(v1, v2))
