24963590fd502aefb1b61318a67ef3bd27376951
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-10-07 11:41:02 -0400

    Added new

---
+++ Labs/lab2_1.py
@@ -0,0 +1,36 @@
+def vector_from_points(p1, p2):
+    ''' Fill in docstring
+    '''
+
+def vector_length(v):
+    ''' Fill in docstring
+    '''
+
+def angle_between(v, w):
+    ''' Fill in docstring
+    '''
+
+def dot_product(v,w):
+    ''' Fill in docstring
+    '''
+
+def unit_vector(v):
+    ''' Fill in docstring
+    '''
+
+def cross_product(v,w):
+    ''' Fill in docstring
+    '''
+
+def scalar_projection(v,w):
+    ''' Fill in docstring
+    '''
+
+def vector_projection(v,w):
+    ''' Fill in docstring
+    '''
+
+if __name__ == "__main__":
+    # test your vector operations here
+    v1 = [0, -2, 3]
+    v2 = [1, 1, 1]
---
+++ Labs/lab2_2.py
@@ -0,0 +1,17 @@
+def packet_size(packet):
+    ''' Fill in docstring
+    '''
+
+def error_indices(packet1, packet2):
+    ''' Fill in docstring
+    '''
+
+def packet_diff(packet1, packet2):
+    ''' Fill in docstring
+    '''
+
+if __name__ == "__main__":
+    # test your bit error rate detector here
+    packet_sent = [0, 1, 1, 1]
+    packet_received = [1, 1, 1, 1]
+~                                    
