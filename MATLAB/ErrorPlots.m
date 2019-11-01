5b0010ae46be019f9b06b2202267a734b9bec347
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-11-01 00:52:27 -0400

    Update ErrorPlots.m

--- MATLAB/ErrorPlots.m
+++ MATLAB/ErrorPlots.m
@@ -5,13 +5,29 @@ i = zeros(91,1);
 i_actual = 14/3 .* ones(91, 1);
 
 for j = 10:100
-    i(j-9) = NumericIntegration(j);
+    i(j-9) = MidpointInt(j);
 end
     
 hold on
 
 plot(v, i)
 plot(v, i_actual)
+
+e = abs(i - i_actual);
+
+% K = -0.0313
+
+eb = zeros(91,1)
+
+for j = 10:100
+    eb(j-9) = 27*0.0313/(24*j^2);
+end
+
+% TBD ERROR WRONG
+hold off
+plot(v, e)
+hold on
+plot(v, eb)
 
 % now for error
 
