9ff48fa9b4fb9151af1c3844f4f7bdf24749c422
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-11-01 11:41:36 -0400

    Update ErrorPlots.m

--- MATLAB/ErrorPlots.m
+++ MATLAB/ErrorPlots.m
@@ -4,14 +4,14 @@
 
 n = 10:100; % values of n
 
-i = arrayfun(@MidpointInt, n); % computed integral values
+im = arrayfun(@MidpointInt, n); % computed integral values
 
 i_actual = ones(1, 91) .* 14/3; % actual integral values
 
-hold on
+figure('Name', 'Midpoint')
 % plot integral vs actual
 
-plot(n, i, 'b--', n, i_actual, 'b');
+plot(n, im, 'b--', n, i_actual, 'b');
 title('$$ \textup{Midpoint Approximation of}\  \int_{0}^{3}\sqrt{x+1} \ dx $$', 'Interpreter', 'latex');
 xlabel('Number of subdivisions (n)')
 ylabel('Approximation')
@@ -20,14 +20,14 @@ axis([0 100 0 inf])
 axis 'auto y'
 
 % second plot
-hold off
-
+figure('Name', 'Error for Midpoint')
 % compute error function based on definition
-err = abs(14/3 - i);
+err = abs(14/3 - im);
 
-% based on second derivative, least upper bound over [0,3] is -1/32
+% based on second derivative, least upper bound of abs(f''(x)) over [0,3]
+% is 1/4
 
-errbound = 1./n.^2 .* 27/32;
+errbound = 27./(24*n.^2) .* 1/4;
 % must be positive? not sure
 
 plot(n, err, 'b', n, errbound, 'b--');
@@ -38,5 +38,32 @@ legend('actual error', 'error bound')
 axis([0 100 0 inf])
 axis 'auto y'
 
+% next, trapezoid
+it = arrayfun(@TrapInt, n); % computed integral values
+
+figure('Name', 'Trapezoidal')
+% plot integral vs actual
+
+plot(n, it, 'b--', n, i_actual, 'b');
+title('$$ \textup{Trapezoidal Approximation of}\  \int_{0}^{3}\sqrt{x+1} \ dx $$', 'Interpreter', 'latex');
+xlabel('Number of subdivisions (n)')
+ylabel('Approximation')
+legend('approximate value', 'exact value')
+axis([0 100 0 inf])
+axis 'auto y'
+
+err = abs(14/3 - it);
+% change error bound
+errbound = 27./(12*n.^2) .* 1/4;
+
+figure('Name', 'Error for Trapezoidal')
+
+plot(n, err, 'b', n, errbound, 'b--');
+title('$$ \textup{Error Bound vs Actual Error} $$', 'Interpreter', 'latex');
+xlabel('Number of subdivisions (n)')
+ylabel('Error')
+legend('actual error', 'error bound')
+axis([0 100 0 inf])
+axis 'auto y'
 
 
