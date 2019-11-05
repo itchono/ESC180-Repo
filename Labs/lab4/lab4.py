d080a42d55d00884f4a256f425fd4982262b0e34
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-11-05 09:39:55 -0500

    Update lab4.py

---
+++ Labs/lab4/lab4.py
@@ -0,0 +1,8 @@
+import utilities
+
+def parse_story(file_name):
+    '''
+    (str) -> list(str)
+
+    parse_story takes in a filename, and returns the text in the file as a list, with bad characters removed.
+    '''
